1c675c62096e5f35df362c558559f28121b8afdc
baseball890912 <baseball.89.0912@gmail.com>
2017-05-18 08:36:58 +0900

    commit

--- FaceCamera/app/src/main/java/com/example/baseb/facecamera/FaceView.java
+++ FaceCamera/app/src/main/java/com/example/baseb/facecamera/FaceView.java
@@ -21,36 +21,10 @@ import android.util.Log;
 import android.view.View;
 
 
-public class FaceView extends View implements Runnable ,SensorEventListener {
+public class FaceView extends View implements Runnable{
 
     Resources res = this.getContext().getResources();
     Bitmap kawamuso = BitmapFactory.decodeResource(res, R.drawable.kawauso);
-
-
-    /** デバッグ用 */
-    private static final boolean DEBUG = true;
-    private static final String TAG = "OrientationListener";
-    /** 行列数 */
-    private static final int MATRIX_SIZE = 16;
-    /** 三次元(XYZ) */
-    private static final int DIMENSION = 3;
-    /** センサー管理クラス */
-    private SensorManager mManager;
-    /** 地磁気行列 */
-    private float[] mMagneticValues;
-    /** 加速度行列 */
-    private float[] mAccelerometerValues;
-    /** X軸の回転角度 */
-    private int mPitchX;
-    /** Y軸の回転角度 */
-    private int mRollY;
-    /** Z軸の回転角度(方位角) */
-    private int mAzimuthZ;
-
-    public synchronized int getAzimuth() {
-        return mAzimuthZ;
-    }
-
 
     /** 塗りつぶし設定の {@link Paint} です。 */
     private Paint mPaint;
@@ -79,8 +53,7 @@ public class FaceView extends View implements Runnable ,SensorEventListener {
         mPaint = new Paint();
         mPaint.setAntiAlias(true);
         mPaint.setDither(true);
-        mPaint.setColor(Color.MAGENTA);
-        mPaint.setAlpha(128);
+        mPaint.setAlpha(200);
         mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
     }
 
@@ -103,26 +76,15 @@ public class FaceView extends View implements Runnable ,SensorEventListener {
             if (face == null) {
                 continue;
             }
-            // 短形で塗りつぶす
             Matrix matrix = new Matrix();
-            // スケールの算出
             matrix.postScale(getWidth() / 2000f, getHeight() / 2000f);
-            // 座標の移動
             matrix.postTranslate(getWidth() / 2f, getHeight() / 2f);
-            // Canvas の保存
             int saveCount = canvas.save();
-            //matrix.postRotate(90.0f);
-            // Matrix を適用
-            matrix.setRotate(mAzimuthZ, kawamuso.getWidth()/2, kawamuso.getHeight()/2);
             canvas.concat(matrix);
-            // 描画
-            //canvas.drawRect(face.rect, mPaint);
 
-            Paint  paint  = new Paint();
-            Rect prect = new Rect(0, 0, kawamuso.getWidth(), kawamuso.getHeight());
-            //canvas.drawBitmap(kawamuso,face.rect.centerX()*2,face.rect.centerY()*2,paint);
-            canvas.drawBitmap(kawamuso,prect,face.rect,paint);
-            // リストア
+            Rect rect = new Rect(0, 0, kawamuso.getWidth(), kawamuso.getHeight());
+            canvas.drawBitmap(kawamuso,rect,face.rect,mPaint);
+
             canvas.restoreToCount(saveCount);
 
 
@@ -132,48 +94,6 @@ public class FaceView extends View implements Runnable ,SensorEventListener {
 
     @Override
     public void run() {
-
-    }
-
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        // センサーイベント
-        switch (event.sensor.getType()) {
-            case Sensor.TYPE_MAGNETIC_FIELD:
-                // 地磁気センサー
-                mMagneticValues = event.values.clone();
-                break;
-            case Sensor.TYPE_ACCELEROMETER:
-                // 加速度センサー
-                mAccelerometerValues = event.values.clone();
-                break;
-            default:
-                // それ以外は無視
-                return;
-        }
-        if (mMagneticValues != null && mAccelerometerValues != null) {
-            float[] rotationMatrix = new float[MATRIX_SIZE];
-            float[] inclinationMatrix = new float[MATRIX_SIZE];
-            float[] remapedMatrix = new float[MATRIX_SIZE];
-            float[] orientationValues = new float[DIMENSION];
-            // 加速度センサーと地磁気センサーから回転行列を取得
-            SensorManager.getRotationMatrix(rotationMatrix, inclinationMatrix, mAccelerometerValues, mMagneticValues);
-            SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_X, SensorManager.AXIS_Z, remapedMatrix);
-            SensorManager.getOrientation(remapedMatrix, orientationValues);
-            // ラジアン値を変換し、それぞれの回転角度を取得する
-            mAzimuthZ = radianToDegrees(orientationValues[0]);
-            mPitchX = radianToDegrees(orientationValues[1]);
-            mRollY = radianToDegrees(orientationValues[2]);
-            if (DEBUG) {
-                Log.d(TAG, "X=" + mPitchX + "Y=" + mRollY + "Z=" + mAzimuthZ);
-            }
-        }
-    }
-    private int radianToDegrees(float angrad) {
-        return (int) Math.floor(angrad >= 0 ? Math.toDegrees(angrad) : 360 + Math.toDegrees(angrad));
-    }
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 }
